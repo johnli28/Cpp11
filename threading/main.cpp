2baee16826840f342ef08afd000e55693eeff8f8
zhuang.li <zhuang.li@thomsonreuters.com>
2018-07-17 11:27:40 +0800

    Add the first threading application and CMakeLists.txt

---
+++ threading/main.cpp
@@ -0,0 +1,38 @@
+// A simple program that computes the square root of a number
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <mutex>
+#include <sched.h>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, const char** argv) {
+  unsigned num_cpus = std::thread::hardware_concurrency();
+  std::cout << "Launching " << num_cpus << " threads\n";
+
+  // A mutex ensures orderly access to std::cout from multiple threads.
+  std::mutex iomutex;
+  std::vector<std::thread> threads(num_cpus);
+  for (unsigned i = 0; i < num_cpus; ++i) {
+    threads[i] = std::thread([&iomutex, i] {
+      {
+        // Use a lexical scope and lock_guard to safely lock the mutex only for
+        // the duration of std::cout usage.
+        //std::lock_guard<std::mutex> iolock(iomutex);
+        std::cout << "Thread #" << i << " is running\n";
+      }
+
+      // Simulate important work done by the tread by sleeping for a bit...
+      std::this_thread::sleep_for(std::chrono::milliseconds(200));
+
+    });
+  }
+
+  for (auto& t : threads) {
+    t.join();
+  }
+  return 0;
+}
